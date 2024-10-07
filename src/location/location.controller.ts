19b0b0686fb0f73be13761aa42d615a7bc42a9b7
Ernor1 <honorerukundo74@gmail.com>
2024-10-07 12:21:27 +0200

    location children

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get , Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { LocationService, LocationWithChildren, LocationWithParent } from './location.service';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { Allow } from 'src/decorators/allow.decorator';
@@ -9,55 +9,63 @@ import { ApiResponse } from 'src/responses/api.response';
 @ApiTags('Location')
 @Allow()
 export class LocationController {
-    constructor( 
-        private readonly locationService : LocationService
-    ){}
+    constructor(
+        private readonly locationService: LocationService
+    ) { }
 
     // / get methods for the location entity 
-    
+
     @Get('/all')
-    async getAll() : Promise<ApiResponse<LocationWithParent[]>> {
-        return new ApiResponse<LocationWithParent[]>(true , "Success" , await this.locationService.getAll() , 200)
+    async getAll(): Promise<ApiResponse<LocationWithParent[]>> {
+        return new ApiResponse<LocationWithParent[]>(true, "Success", await this.locationService.getAll(), 200)
     }
 
     @Get('/:id')
     @ApiParam({
-        name : "id",
-        type : Number
+        name: "id",
+        type: Number
     })
-    async getLocationById( @Param('id') id: number ) : Promise<ApiResponse<Location>> {
-        return new ApiResponse<Location>(true , "Success" , await this.locationService.getLocationById(id) , 200);
+    async getLocationById(@Param('id') id: number): Promise<ApiResponse<Location>> {
+        return new ApiResponse<Location>(true, "Success", await this.locationService.getLocationById(id), 200);
     }
 
     @Get('/children/:id')
     @ApiParam({
-        name : "id",
-        type : Number
+        name: "id",
+        type: Number
     })
-    async getChildrenLocations(@Param('id') id : number) : Promise<ApiResponse<LocationWithChildren[]>> {
-        return new ApiResponse<LocationWithChildren[]>(true , "Success" , await this.locationService.getChildrenLocations(id) , 200)
+    async getChildrenLocations(@Param('id') id: number): Promise<ApiResponse<LocationWithChildren[]>> {
+        return new ApiResponse<LocationWithChildren[]>(true, "Success", await this.locationService.getChildrenLocations(id), 200)
     }
 
     @Get('/level/:id')
     @ApiParam({
-        name : "id",
-        type : Number
+        name: "id",
+        type: Number
     })
-    async getLocationByLevel(@Param('id') id : number) : Promise<ApiResponse<Location[]>> {
-        return new ApiResponse<Location[]>(true , "Success" , await this.locationService.getLocationByLevel(id) , 200)
+    async getLocationByLevel(@Param('id') id: number): Promise<ApiResponse<Location[]>> {
+        return new ApiResponse<Location[]>(true, "Success", await this.locationService.getLocationByLevel(id), 200)
     }
 
     @Get('/location-levels/all')
-    async getAllLocationLevels() : Promise<ApiResponse<locationLevel[]>> {
-        return new ApiResponse<locationLevel[]>(true , "Success" , await this.locationService.getAllLocationLevels() , 200)
+    async getAllLocationLevels(): Promise<ApiResponse<locationLevel[]>> {
+        return new ApiResponse<locationLevel[]>(true, "Success", await this.locationService.getAllLocationLevels(), 200)
     }
 
     @Get('/location-levels/:id')
     @ApiParam({
-        name : "id",
-        type : Number
+        name: "id",
+        type: Number
     })
-    async getLOcationLevelById(@Param("id") id : number) : Promise<ApiResponse<locationLevel>> {
-        return new ApiResponse<locationLevel>(true , "Success" , await this.locationService.getLOcationLevelById(id) , 200)
+    async getLOcationLevelById(@Param("id") id: number): Promise<ApiResponse<locationLevel>> {
+        return new ApiResponse<locationLevel>(true, "Success", await this.locationService.getLOcationLevelById(id), 200)
+    }
+    @Get('/all/children')
+    @ApiParam({
+        name: "id",
+        type: Number
+    })
+    async getAllWithChildren(@Param("id") id: number): Promise<ApiResponse<LocationWithChildren[]>> {
+        return new ApiResponse<any>(true, "Success", await this.locationService.getAllChildrenLocations(id), 200)
     }
 }
