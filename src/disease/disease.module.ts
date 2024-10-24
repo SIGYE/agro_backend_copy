b434153603b9ada6b2c49e9e1a327875266a0e2f
Ernor1 <honorerukundo74@gmail.com>
2024-10-24 17:23:20 +0200

    [FIX]

--- src/disease/disease.module.ts
+++ src/disease/disease.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DiseaseService } from './disease.service';
 import { DiseaseController } from './disease.controller';
+import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
+import { UsersService } from 'src/users/users.service';
+import { LocationService } from 'src/location/location.service';
 
 @Module({
   controllers: [DiseaseController],
-  providers: [DiseaseService],
+  providers: [DiseaseService, JwtService, Reflector, UsersService, LocationService],
 })
-export class DiseaseModule {}
+export class DiseaseModule { }
