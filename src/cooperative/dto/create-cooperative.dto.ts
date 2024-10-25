96c61ff327493803f69acc61ad0877ecb4127801
Ernor1 <honorerukundo74@gmail.com>
2024-10-25 12:33:56 +0200

    [FIX] cooperative dto

--- src/cooperative/dto/create-cooperative.dto.ts
+++ src/cooperative/dto/create-cooperative.dto.ts
@@ -1,23 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsInt, IsOptional } from 'class-validator';
 
 export class CreateCooperativeDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     telephone: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     registrationNumber: string;
 
     @IsInt()
     @IsNotEmpty()
+    @ApiProperty()
     membersNumber: number;
 
     @IsInt()
     @IsNotEmpty()
+    @ApiProperty()
     locationId: number;
 }
