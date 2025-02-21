ec4ba50ebe52ff10ae06451cccdf151212702a9f
Ernor1 <honorerukundo74@gmail.com>
2025-02-21 16:51:36 +0200

    [FIX] assign pest dto

--- src/pests/dto/assign-pest.dto.ts
+++ src/pests/dto/assign-pest.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class AssignPestDto {
+    @ApiProperty()
     pestId: string
+    @ApiProperty()
     crops: string[]
+    @ApiProperty()
     animals: string[]
 }
